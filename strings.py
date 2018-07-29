0d631e9004b17bdbb3f5f6dd5214833ae5cc65d8
Jess <z5206673@student.unsw.edu.au>
2018-07-29 17:52:03 +1000

    printed string of text

--- strings.py
+++ strings.py
@@ -1,2 +1,11 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
+
+x = 0
+for string in strings:
+    print(strings[x], end = " ")
+    x = x + 1
+
+print(sentence)
+
+print(' '.join(strings))
